3e30998049cd5f8c369950063f58b0ed7c7ae1b9
Tony L <708162@pdsb.net>
2023-02-22 08:59:25 -0500

    feat: add alternative s2 solution

--- 2023/s2.py
+++ 2023/s2.py
@@ -17,3 +17,25 @@ for j in range(1, N):
             ans[j - i] = grid[i][j]
 
 print(" ".join(str(i) for i in ans))
+
+# Below is an alternative solution
+
+# CCC '23 S2 - William Shen
+n = int(input())
+heights = list(map(int, input().split()))
+ans = [1000000000000] * (n + 5)
+for i in range(n):
+    cur = 0
+    for j in range(n + 1):
+        if i - j < 0 or i + j >= n:
+            break
+        cur += abs(heights[i - j] - heights[i + j])
+        ans[2 * j + 1] = min(ans[2 * j + 1], cur)
+for i in range(n):
+    cur = 0
+    for j in range(n + 1):
+        if i - j < 0 or i + j + 1>= n:
+            break
+        cur += abs(heights[i - j] - heights[i + j + 1])
+        ans[2 * j + 2] = min(ans[2 * j + 2], cur)
+print(*ans[1:n + 1])
